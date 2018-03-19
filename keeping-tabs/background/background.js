2b8b410a5711da489bc276cc23ad7cda052d45f1
Marcus Mu <chunkhang@gmail.com>
2018-03-19 22:42:01 +0800

    Create one manager tab

--- keeping-tabs/background/background.js
+++ keeping-tabs/background/background.js
@@ -1,14 +1,38 @@
+var managerTabId = null;
+
 // Open extension page when toolbar button is clicked
 browser.browserAction.onClicked.addListener(function() {
-  // Create tab for extension page
-  browser.tabs.create({
-    url: "page/manager.html",
-    active: true
-  }).then(function(tab) {
-    // Tab created
-    console.log(`Created tab: ${tab.id}`);
-  }, function(error) {
-    // Handle error
-    console.log(`Error: ${error}`);
+  // Check if manager tab already open
+  checkTabIsOpen(managerTabId).then(function(open) {
+    if (open) {
+      // Switch to manager tab
+      browser.tabs.update(managerTabId, {active: true});
+    } else {
+      // Create manager tab
+      browser.tabs.create({
+        url: "page/manager.html",
+        active: true
+      }).then(function(tab) {
+        // Tab created
+        managerTabId = tab.id;
+      });
+    }
   });
 });
+
+// Check if tab with given id is open in current window
+function checkTabIsOpen(tabId) {
+  return new Promise(function(resolve, reject) {
+    browser.tabs.query({
+      currentWindow: true
+    }).then(function(tabs) {
+      if (tabs.filter(tab => tab.id == tabId).length == 1) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    }, function(error) {
+      reject(error);
+    });
+  });
+}
